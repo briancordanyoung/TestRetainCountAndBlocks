eae2c842aaa35e64f49ac134b4cacfa57a74f389
Brian Young <bdy-github@hiddentower.com>
2015-05-28 09:26:10 -0700

    Initial Commit

---
+++ testRetainCountAndBlocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testRetainCountAndBlocks
+//
+//  Created by Brian Cordan Young on 5/28/15.
+//  Copyright (c) 2015 Brian Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testRetainCountAndBlocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testRetainCountAndBlocks
+//
+//  Created by Brian Cordan Young on 5/28/15.
+//  Copyright (c) 2015 Brian Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
